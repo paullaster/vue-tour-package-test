e2aa275261ab4137e1d2e2d0ce72df0c358b3c26
paullaster <paullasterokoth98@gmail.com>
2023-03-16 17:01:22 +0300

    add files

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app">
     <h1>Testing vue tour package for vue v2</h1>
+    <div id="id1"> This is the first place</div>
+    <div id="id2"> This is the second place</div>
+    <div id="id3"> This is the third place</div>
+    <div id="id4"> This is the fourth place</div>
+    <div id="id5"> this is the last place</div>
   </div>
 </template>
 
 <script>
-import {}
+
 </script>
