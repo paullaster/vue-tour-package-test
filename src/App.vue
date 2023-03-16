4569795054c7e3bfdefefda33691a3388a7c99a9
paullaster <paullasterokoth98@gmail.com>
2023-03-16 16:59:08 +0300

    add files

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,5 @@
 </template>
 
 <script>
-
+import {}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueTour from 'vue-tour'
+
+require('vue-tour/dist/vue-tour.css')
+
+Vue.use(VueTour)
 
 Vue.config.productionTip = false
 
